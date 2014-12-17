00cc75211868d57a15d84ffdf5f12819d86b97f9
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-12-17 12:41:42 -0500

    firsties

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The theme header
+ * 
+ * @package bootstrap-basic
+ */
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>  <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>     <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>     <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title('|', true, 'right'); ?></title>
+		<meta name="viewport" content="width=device-width">
+
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		
+		<!--wordpress head-->
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<!--[if lt IE 8]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+		
+		
+		<div class = "container-fluid">
+		
+		
+		
+			<?php do_action('before'); ?> 
+			<header role="banner">
+				<div class = "container-fluid">
+					<div class ="col-md-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" /></div>
+					<div class ="col-md-4"><h1>Doing great media because <br><small>you know, stuff - brah</small></div>
+				</div>
+			
+			</header>
+			
+		</div>
+		
+				<!-- end header.php -->
